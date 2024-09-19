905faff5a86a2c4bade3ce613a9a362b05f57ab6
AMIT KUMAR DUYA <amitkumarduya35@gmail.com>
2024-09-20 00:32:54 +0530

    new

---
+++ dubollikelist.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node {
+    struct node *pre;
+    int data;
+    struct node *next;
+};
+
+struct node *head = NULL;
+
+void add_of_bigenig() {
+    struct node *nw;
+    int num;
+    int ch=1;
+    while (ch)
+    {
+
+    printf("Enter the number to add at the beginning: ");
+    scanf("%d", &num);
+    nw = (struct node*)malloc(sizeof(struct node));
+ 
+    nw->data = num;
+    nw->next = head;
+    nw->pre = NULL;
+    
+    head = nw;
+    
+    printf("Enter the 1 or 0 1 mins agian add & 0 mins on add :");
+    scanf("%d",&ch);
+    }
+}
+void append(){
+    struct node *temp,*nw;
+    int num;
+    printf("Enter the number of append in list :");
+    scanf("%d",&num);
+    nw=
+}
+
+void display() {
+    struct node *temp = head;
+    if (head == NULL) {
+        printf("List is empty.\n");
+        return;
+    }
+    printf("The list is: ");
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    add_of_bigenig();
+    display();
+    return 0;
+}
